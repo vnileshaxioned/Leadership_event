5ef62b66625b977943fcd31d5c45931cadfc5a5a
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:23:11 +0530

    added event speaker section to template part home page

---
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-event-speaker.php
@@ -0,0 +1,13 @@
+<section class="event-speaker" style="background-image: url('assets/images/terren-hurst-blgOFmPIlr0-unsplash.jpg');">
+  <div class="wrapper">
+    <ul class="event-speaker-content">
+      <li class="left-speaker-content">
+        <h4 class="section-heading">Become an <span class="heading-color">event speaker?</span></h4>
+        <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore</p>
+      </li>
+      <li class="right-speaker-content">
+        <a href="#FIXME" class="event-register-button" title="Register today">Register today</a>
+      </li>
+    </ul>
+  </div>
+</section>
