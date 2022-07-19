9fd063f8579211fe51673cd40c4d7db48364cbff
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:26:27 +0530

    added contact section to template part home page

---
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-contact.php
@@ -0,0 +1,26 @@
+<section class="contact" style="background-image: url('assets/images/maria-stewart-p4tj0g-_aMM-unsplash.jpg');">
+  <div class="wrapper">
+    <div class="contact-content">
+      <h5 class="contact-heading">Please say hi</h5>
+      <form action="#FIXME" class="contact-form" method="post">
+        <div class="form-container">
+          <div class="form-group">
+            <input type="text" name="name" class="form-content" placeholder="Name">
+          </div>
+          <div class="form-group">
+            <input type="text" name="email" class="form-content" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <input type="text" name="subject" class="form-content" placeholder="Subject">
+          </div>
+        </div>
+        <div class="form-textarea">
+          <textarea name="message" cols="30" rows="5" placeholder="Message"></textarea>
+        </div>
+        <div class="form-textarea">
+          <input type="submit" name="contact_submit" class="contact-submit-button" value="Submit">
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
