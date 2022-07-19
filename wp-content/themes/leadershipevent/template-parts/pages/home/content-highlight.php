68ff58e58dddac61eb1cfe5151752509ba04c930
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:08:56 +0530

    added highlight section to template part home page

---
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-highlight.php
@@ -0,0 +1,30 @@
+<section class="highlight">
+  <div class="wrapper">
+    <ul class="highlight-container">
+      <li class="highlight-list">
+        <figure class="highlight-image">
+          <img src="assets/images/highlight/alexandre-pellaes-6vAjp0pscX0-unsplash.jpg" alt="Alexandre Pellaes" />
+        </figure>
+        <h3 class="highlight-heading">
+          <a href="#FIXME" class="highlight-link">2019 Highlights</a>
+        </h3>
+      </li>
+      <li class="highlight-list">
+        <figure class="highlight-image">
+          <img src="assets/images/highlight/miguel-henriques--8atMWER8bI-unsplash.jpg" alt="Miguel Henriques" />
+        </figure>
+        <h3 class="highlight-heading">
+          <a href="#FIXME" class="highlight-link">2020 Highlights</a>
+        </h3>
+      </li>
+      <li class="highlight-list">
+        <figure class="highlight-image">
+          <img src="assets/images/highlight/jakob-dalbjorn-cuKJre3nyYc-unsplash.jpg" alt="Jakob Dalbjorn" />
+        </figure>
+        <h3 class="highlight-heading">
+          <a href="#FIXME" class="highlight-link">2021 Highlights</a>
+        </h3>
+      </li>
+    </ul>
+  </div>
+</section>
