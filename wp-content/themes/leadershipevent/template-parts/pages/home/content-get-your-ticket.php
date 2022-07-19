2d47059d4d54d1230a23ab432a165053088647e6
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:24:30 +0530

    added get your ticket section to template part home page

---
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-get-your-ticket.php
@@ -0,0 +1,76 @@
+<section class="get-your-tickets">
+  <div class="wrapper">
+    <h4 class="section-heading">Get your <span class="heading-color">tickets</span></h4>
+    <ul class="ticket-content">
+      <li class="ticket-content-list">
+        <div class="ticket-head">
+          <h5 class="ticket-heading">Early bird</h5>
+          <span class="ticket-price">$640</span>
+        </div>
+        <ul class="ticket-container">
+          <li class="ticket-list">
+            <p class="paragraph">All-Day Coffee + Snacks</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">After Party</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">24/7 Support</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">Quick group meetings for multiple teams</p>
+          </li>
+          <li class="ticket-list">
+            <a href="#FIXME" class="buy-ticket-button" title="Buy tickets">Buy tickets</a>
+          </li>
+        </ul>
+      </li>
+      <li class="ticket-content-list">
+        <div class="ticket-head">
+          <h5 class="ticket-heading">Early bird</h5>
+          <span class="ticket-price">$640</span>
+        </div>
+        <ul class="ticket-container">
+          <li class="ticket-list">
+            <p class="paragraph">All-Day Coffee + Snacks</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">Group Meetings + After Party</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">24/7 Support + Instant Chats</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">Quick group meetings for multiple teams</p>
+          </li>
+          <li class="ticket-list">
+            <a href="#FIXME" class="buy-ticket-button" title="Buy tickets">Buy tickets</a>
+          </li>
+        </ul>
+      </li>
+      <li class="ticket-content-list">
+        <div class="ticket-head">
+          <h5 class="ticket-heading">Early bird</h5>
+          <span class="ticket-price">$640</span>
+        </div>
+        <ul class="ticket-container">
+          <li class="ticket-list">
+            <p class="paragraph">Cashback $200</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">Private Meetings + After Party</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">24/7 Support + Instant Chats</p>
+          </li>
+          <li class="ticket-list">
+            <p class="paragraph">Quick group meetings for multiple teams</p>
+          </li>
+          <li class="ticket-list">
+            <a href="#FIXME" class="buy-ticket-button" title="Buy tickets">Buy tickets</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</section>
