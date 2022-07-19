80fe8f2cf11fafb8452c6f270476ed8baa85d553
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:00:56 +0530

    footer section added

---
+++ wp-content/themes/leadershipevent/footer.php
@@ -0,0 +1,40 @@
+    </main>
+    <!--main section end-->
+    <!--footer section start-->
+    <footer>
+      <div class="wrapper">
+        <div class="top-footer">
+          <h6 class="footer-heading"><a href="/index.html" class="main-logo" title="Leadership event">Leadership event</a></h6>
+          <ul class="footer-social-icon">
+            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Facebook">Facebook</a></li>
+            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Instagram">Instagram</a></li>
+            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Whatsapp">Whatsapp</a></li>
+            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Youtube">Youtube</a></li>
+          </ul>
+        </div>
+        <div class="bottom-footer">
+          <nav>
+            <ul class="footer-navbar">
+              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Our story">Our story</a></li>
+              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Code of Conduct">Code of Conduct</a></li>
+              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Privacy and Terms">Privacy and Terms</a></li>
+              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Contact">Contact</a></li>
+            </ul>
+          </nav>
+          <div class="bottom-footer-content">
+            <p class="paragraph">Copyright &copy; 2022 Leadership Event Co., Ltd. All Rights Reserved.</p>
+            <p class="paragraph">Design: <a href="#FIXME" target="_blank" class="credit" title="TemplateMo">TemplateMo</a></p>
+          </div>
+          <div class="go-to-top">
+            <a href="#FIXME" class="top-button" title="Go to top">Go to top</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!--footer section end-->
+  </div>
+  <!--container end-->
+<script src="assets/vendor/jquery-1.8.3.min.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
