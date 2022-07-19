8ad9217ff2a21412ddce0662696b250887ec67a2
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:04:13 +0530

    fix html format structure

--- wp-content/themes/leadershipevent/footer.php
+++ wp-content/themes/leadershipevent/footer.php
@@ -4,21 +4,39 @@
     <footer>
       <div class="wrapper">
         <div class="top-footer">
-          <h6 class="footer-heading"><a href="/index.html" class="main-logo" title="Leadership event">Leadership event</a></h6>
+          <h6 class="footer-heading">
+            <a href="/index.html" class="main-logo" title="Leadership event">Leadership event</a>
+          </h6>
           <ul class="footer-social-icon">
-            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Facebook">Facebook</a></li>
-            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Instagram">Instagram</a></li>
-            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Whatsapp">Whatsapp</a></li>
-            <li class="footer-social-list"><a href="#FIXME" class="footer-icon" target="_blank" title="Youtube">Youtube</a></li>
+            <li class="footer-social-list">
+              <a href="#FIXME" class="footer-icon" target="_blank" title="Facebook">Facebook</a>
+            </li>
+            <li class="footer-social-list">
+              <a href="#FIXME" class="footer-icon" target="_blank" title="Instagram">Instagram</a>
+            </li>
+            <li class="footer-social-list">
+              <a href="#FIXME" class="footer-icon" target="_blank" title="Whatsapp">Whatsapp</a>
+            </li>
+            <li class="footer-social-list">
+              <a href="#FIXME" class="footer-icon" target="_blank" title="Youtube">Youtube</a>
+            </li>
           </ul>
         </div>
         <div class="bottom-footer">
           <nav>
             <ul class="footer-navbar">
-              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Our story">Our story</a></li>
-              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Code of Conduct">Code of Conduct</a></li>
-              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Privacy and Terms">Privacy and Terms</a></li>
-              <li class="footer-menu-list"><a href="#FIXME" class="footer-menu" title="Contact">Contact</a></li>
+              <li class="footer-menu-list">
+                <a href="#FIXME" class="footer-menu" title="Our story">Our story</a>
+              </li>
+              <li class="footer-menu-list">
+                <a href="#FIXME" class="footer-menu" title="Code of Conduct">Code of Conduct</a>
+              </li>
+              <li class="footer-menu-list">
+                <a href="#FIXME" class="footer-menu" title="Privacy and Terms">Privacy and Terms</a>
+              </li>
+              <li class="footer-menu-list">
+                <a href="#FIXME" class="footer-menu" title="Contact">Contact</a>
+              </li>
             </ul>
           </nav>
           <div class="bottom-footer-content">
