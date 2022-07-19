252b72830bf32e6973d77a5727f801ad9123ac5f
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:07:05 +0530

    added banner section to template part home page

---
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-banner.php
@@ -0,0 +1,18 @@
+<section class="banner">
+  <div class="wrapper">
+    <video class="banner-video" autoplay muted loop>
+      <source src="assets/videos/pexels-pavel-danilyuk-8716790.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+    <div class="banner-content">
+      <h2 class="banner-heading"><span class="heading-color">Leadership</span> Conference 2022</h2>
+      <div class="date-location">
+        <span class="date">July 12 to 18, 2022</span>
+        <span class="location">Times Square, NY</span>
+      </div>
+      <div class="slide-down">
+        <a href="#FIXME" class="slide-down-button" title="Slide down">Slide down</a>
+      </div>
+    </div>
+  </div>
+</section>
