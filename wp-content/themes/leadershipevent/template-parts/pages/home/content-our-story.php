9429e49c3fcd8711111299504642a64bb0c7a066
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:18:37 +0530

    added our story section to template part home page

---
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-our-story.php
@@ -0,0 +1,47 @@
+<section class="our-story">
+  <div class="wrapper">
+    <h4 class="section-heading">Our <span class="heading-color">story</span></h4>
+    <ul class="story-content">
+      <li class="leadership-conference">
+        <h5 class="content-heading">The importance of Leadership Conference in 2022</h5>
+        <p class="paragraph">Leadership Event is one-page Bootstrap v5.1.3 CSS layout for your website. Thank you for choosing TemplateMo website where you can instantly download free CSS templates at no cost.</p>
+        <ul class="action-container">
+          <li class="action-links">
+            <a href="#FIXME" class="action-speaker-button action-button" title="Meet speakers">Meet speakers</a>
+          </li>
+          <li class="action-links">
+            <a href="#FIXME" class="action-schedule-button action-button" title="Check out schedule">Check out Schedule</a>
+          </li>
+        </ul>
+      </li>
+      <li class="leadership-conference">
+        <h5 class="medium-content-heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore</h5>
+        <ul class="people-container">
+          <li class="people-list">
+            <figure class="people-image">
+              <img src="assets/images/avatar/portrait-good-looking-brunette-young-asian-woman.jpg" alt="Asian Woman">
+            </figure>
+          </li>
+          <li class="people-list">
+            <figure class="people-image">
+              <img src="assets/images/avatar/happy-asian-man-standing-with-arms-crossed-grey-wall.jpg" alt="Asian Man">
+            </figure>
+          </li>
+          <li class="people-list">
+            <figure class="people-image">
+              <img src="assets/images/avatar/senior-man-white-sweater-eyeglasses.jpg" alt="Senior Man">
+            </figure>
+          </li>
+          <li class="people-list">
+            <figure class="people-image">
+              <img src="assets/images/avatar/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction.jpg" alt="Joyfully Female">
+            </figure>
+          </li>
+          <li class="people-list">
+            <p class="paragraph">120+ People are attending with us</p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</section>
