bfbc7380cf13991166a575555798572224d7e560
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-19 12:20:29 +0530

    added our speaker section to template part home page

---
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-our-speaker.php
@@ -0,0 +1,127 @@
+<section class="our-speaker">
+  <div class="wrapper">
+    <ul class="speaker-top-container">
+      <li class="speaker-content">
+        <h4 class="section-heading">Our <span class="heading-color">speakers</span></h4>
+        <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore</p>
+      </li>
+      <li class="speaker-content">
+        <figure>
+          <img src="assets/images/avatar/happy-asian-man-standing-with-arms-crossed-grey-wall.jpg" alt="Asian Man">
+          <figcaption>Featured</figcaption>
+        </figure>
+        <ul class="speaker-inner-content">
+          <li class="speaker-content-list">
+            <h5 class="speaker-content-heading">Logan wilson</h5>
+            <p class="speaker-position">CEO / founder</p>
+          </li>
+          <li class="speaker-content-list">
+            <ul class="social-links">
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon facebook" target="_blank" title="Facebook">Facebook</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon instagram" target="_blank" title="Instagram">Instagram</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon google" target="_blank" title="Google">Google</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="speaker-bottom-container">
+      <li class="speaker-image-list">
+        <figure class="speaker-imge">
+          <img src="assets/images/avatar/portrait-good-looking-brunette-young-asian-woman.jpg" alt="Asian Woman">
+        </figure>
+        <ul class="speaker-inner-content">
+          <li class="speaker-content-list">
+            <h5 class="speaker-content-heading">Natalie</h5>
+            <p class="speaker-position">Event planner</p>
+          </li>
+          <li class="speaker-content-list">
+            <ul class="social-links">
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon facebook" target="_blank" title="Facebook">Facebook</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon instagram" target="_blank" title="Instagram">Instagram</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li class="speaker-image-list">
+        <figure class="speaker-imge">
+          <img src="assets/images/avatar/senior-man-white-sweater-eyeglasses.jpg" alt="Senior Man">
+        </figure>
+        <ul class="speaker-inner-content">
+          <li class="speaker-content-list">
+            <h5 class="speaker-content-heading">Thomas</h5>
+            <p class="speaker-position">Startup coach</p>
+          </li>
+          <li class="speaker-content-list">
+            <ul class="social-links">
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon instagram" target="_blank" title="Instagram">Instagram</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon whatsapp" target="_blank" title="Whatsapp">Whatsapp</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li class="speaker-image-list">
+        <figure class="speaker-imge">
+          <img src="assets/images/avatar/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction.jpg" alt="Pretty Smiling Joyfully Female">
+        </figure>
+        <ul class="speaker-inner-content">
+          <li class="speaker-content-list">
+            <h5 class="speaker-content-heading">Isabella</h5>
+            <p class="speaker-position">Event manager</p>
+          </li>
+          <li class="speaker-content-list">
+            <ul class="social-links">
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon facebook" target="_blank" title="Facebook">Facebook</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon instagram" target="_blank" title="Instagram">Instagram</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon whatsapp" target="_blank" title="Whatsapp">Whatsapp</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li class="speaker-image-list">
+        <figure class="speaker-imge">
+          <img src="assets/images/avatar/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university.jpg" alt="Happy African American Woman">
+        </figure>
+        <ul class="speaker-inner-content">
+          <li class="speaker-content-list">
+            <h5 class="speaker-content-heading">Samantha</h5>
+            <p class="speaker-position">Top level speaker</p>
+          </li>
+          <li class="speaker-content-list">
+            <ul class="social-links">
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon facebook" target="_blank" title="Facebook">Facebook</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon instagram" target="_blank" title="Instagram">Instagram</a>
+              </li>
+              <li class="social-icon-list">
+                <a href="#FIXME" class="social-icon whatsapp" target="_blank" title="Whatsapp">Whatsapp</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</section>
