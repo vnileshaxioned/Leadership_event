da3ba66c03434aaefb408bd16a02cbd76a17ebd9
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-21 18:05:45 +0530

    added script file in js folder

--- wp-content/themes/leadershipevent/functions.php
+++ wp-content/themes/leadershipevent/functions.php
@@ -3,8 +3,8 @@
 // enqueue the style and script files
 add_action('wp_enqueue_scripts', 'test_theme_script');
 function test_theme_script() {
-wp_enqueue_style('custom-styling', get_stylesheet_uri());
-wp_enqueue_style('custom-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('custom-styling', get_stylesheet_uri());
+  wp_enqueue_style('custom-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
 }
 
 // theme support
