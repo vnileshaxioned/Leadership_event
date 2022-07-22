5da0b54876fc400950103273334bd98164ae3acd
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-22 18:44:55 +0530

    added highlight video modal in banner section template parts

--- wp-content/themes/leadershipevent/template-parts/pages/home/content-banner.php
+++ wp-content/themes/leadershipevent/template-parts/pages/home/content-banner.php
@@ -6,8 +6,9 @@
   $video_mime_type = $banner_content['video']['mime_type'];
   $event_date_from = DateTime::createFromFormat('Ymd', $banner_content['event_date_from'])->format('F j');
   $event_date_to = DateTime::createFromFormat('Ymd', $banner_content['event_date_to'])->format('j, Y');
+  $contents = $banner_content['highlight_contents'];
 
-  if ($title || $event_date_from || $event_date_to || $event_location || $video_url) { ?>
+  if ($title || $event_date_from || $event_date_to || $event_location || $video_url || $contents) { ?>
   <section class="banner">
     <div class="wrapper">
       <?php if ($video_url) { ?>
@@ -16,7 +17,7 @@
           Your browser does not support the video tag.
         </video>
       <?php } 
-        if ($title || $event_date_from || $event_date_to || $event_location) { ?>
+        if ($title || $event_date_from || $event_date_to || $event_location || $contents) { ?>
         <div class="banner-content">
           <?php echo $title ? '<div class="wysiwyg-editor">'.$title.'</div>' : null; 
             if ($event_date_from || $event_date_to || $event_location) { ?>
@@ -31,7 +32,41 @@
             <a href="#FIXME" class="slide-down-button" title="Slide down">Slide down</a>
           </div>
         </div>
-      <?php } ?>
-    </div>
-  </section>
-<?php } ?>
+        <ul class="highlight-container">
+        <?php
+          foreach ($contents as $content) {
+            $title = $content['title'];
+            $image_url = $content['image']['url'] ? $content['image']['url'] : null;
+            $image_alt = $content['image']['alt'] ? $content['image']['alt'] : $title;
+
+            if ($title || $image_url) { ?>
+            <li class="highlight-list">
+              <?php if ($image_url) { ?>
+                <figure class="highlight-image">
+                  <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" />
+                </figure>
+              <?php } 
+                echo $title ? '<h3 class="highlight-heading"><a href="#FIXME" class="highlight-link">'.$title.'</a></h3>' : null; ?>
+            </li>
+          <?php }
+            } ?>
+        </ul>
+        <?php
+          foreach ($contents as $content) {
+            $title = $content['title'];
+            $video_url = $content['video']['url'] ? $content['video']['url'] : null;
+            $video_mime_type = $content['video']['mime_type'];
+
+            if ($video_url) { ?>
+            <div class="video-modal">
+              <video class="highlight-video" controls>
+                <source src="<?php echo $video_url; ?>" type="<?php echo $video_mime_type; ?>">
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          <?php }
+            }
+          } ?>
+      </div>
+    </section>
+  <?php } ?>
